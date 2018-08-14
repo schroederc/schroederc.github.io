98f12d78b1c6f70d34fe49538fa6ad2a53ceec5d
Cody Schroeder <schroederc@google.com>
2018-08-14 13:24:58 -0700

    Embed last modified time

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 #!/bin/zsh -e
+cat >'wiki/tiddlers/$__SiteSubtitle.tid' <<EOF
+title: $:/SiteSubtitle
+type: text/vnd.tiddlywiki
+
+EOF
+echo "Last updated: $(date '+%Y/%m/%d %H:%M')" >>'wiki/tiddlers/$__SiteSubtitle.tid'
 tiddlywiki wiki --build index
 mv wiki/output/* .
